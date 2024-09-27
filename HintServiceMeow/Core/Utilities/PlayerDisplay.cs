30da54a4a1a372e8a3e5c85d4d1f3edf04870455
MeowServer <2679977872@qq.com>
2024-09-27 00:11:08 -0400

    Remake updater

--- HintServiceMeow/Core/Utilities/PlayerDisplay.cs
+++ HintServiceMeow/Core/Utilities/PlayerDisplay.cs
@@ -15,6 +15,7 @@ using HintServiceMeow.Core.Utilities.Parser;
 
 //Plugin API
 using Log = PluginAPI.Core.Log;
+using Exiled.API.Features;
 
 
 namespace HintServiceMeow.Core.Utilities
@@ -56,107 +57,23 @@ namespace HintServiceMeow.Core.Utilities
         private Task<string> _currentParserTask;
         private readonly object _currentParserTaskLock = new object();
 
-        #region Update Rate Management
-
-        private readonly object _rateDataLock = new object();
-
         /// <summary>
         /// The text that was sent to the client in latest sync
         /// </summary>
         private string _lastText = string.Empty;
 
-        /// <summary>
-        /// Contains all the hints that had been arranged to update. Make sure that a hint's update time will not be calculated for twice
-        /// </summary>
-        private readonly HashSet<AbstractHint> _updatingHints = new HashSet<AbstractHint>();
-
-        /// <summary>
-        /// The time of latest update
-        /// </summary>
-        private DateTime _lastTimeUpdate = DateTime.MinValue;
-
-        /// <summary>
-        /// The time of second-last update
-        /// </summary>
-        private DateTime _secondLastTimeUpdate = DateTime.MinValue;
-
-        /// <summary>
-        /// The time of next update
-        /// </summary>
+        private readonly UpdateScheduler _updateScheduler = new UpdateScheduler();
+        private DateTime _arrangedUpdateTime = DateTime.MaxValue;
         private DateTime _planUpdateTime = DateTime.MaxValue;
 
-        /// <summary>
-        /// The time when the player display will plan for an update
-        /// </summary>
-        private DateTime _arrangedUpdateTime = DateTime.MaxValue;
-
-        /// <summary>
-        /// The minimum interval between a regular update and the last update
-        /// </summary>
-        private static readonly TimeSpan UpdateInterval = TimeSpan.FromSeconds(0.5f);
-
-        /// <summary>
-        /// The minimum interval between a force update and the second last update
-        /// </summary>
-        private static readonly TimeSpan SecondLastUpdateInterval = TimeSpan.FromSeconds(1f);
-
-        /// <summary>
-        /// The player display will be forced to update when there haven't been any update in this amount of time
-        /// </summary>
-        private static readonly TimeSpan PeriodicUpdateInterval = TimeSpan.FromSeconds(5f);
-
-        /// <summary>
-        /// The time left for regular update to cool down.
-        /// </summary>
-        private TimeSpan UpdateCoolDown
-        {
-            get
-            {
-                TimeSpan delay1 = (this._lastTimeUpdate + UpdateInterval - DateTime.Now);
-                TimeSpan delay2 = (this._secondLastTimeUpdate + SecondLastUpdateInterval - DateTime.Now);
-                return delay1 > delay2 ? delay1 : delay2;
-            }
-        }
-
-        /// <summary>
-        /// The time left for fast update to cool down.
-        /// </summary>
-        private TimeSpan FastUpdateCoolDown => this._secondLastTimeUpdate + SecondLastUpdateInterval - DateTime.Now;
-
-        /// <summary>
-        /// The time PlayerDisplay will execute a periodic update
-        /// </summary>
-        private TimeSpan PeriodicUpdateCoolDown => _lastTimeUpdate + PeriodicUpdateInterval - DateTime.Now;
-
-        /// <summary>
-        /// Whether PlayerDisplay need to force update or not
-        /// </summary>
-        private bool NeedPeriodicUpdate => PeriodicUpdateCoolDown.Ticks <= 0;
-
-        /// <summary>
-        /// Whether regular update is ready
-        /// </summary>
-        private bool UpdateReady => UpdateCoolDown.Ticks <= 0;
-
-        /// <summary>
-        /// Whether fast update is ready
-        /// </summary>
-        private bool FastUpdateReady => FastUpdateCoolDown.Ticks <= 0;
+        #region Update Rate Management
 
         internal void OnHintUpdate(AbstractHint hint)
         {
-            lock (_rateDataLock)
-            {
-                if (_updatingHints.Contains(hint))
-                    return;
-
-                _updatingHints.Add(hint);
-            }
-
             switch (hint.SyncSpeed)
             {
                 case HintSyncSpeed.Fastest:
-                    UpdateWhenAvailable(true);
+                    _updateScheduler.UpdateWhenAvailable(true);
                     break;
                 case HintSyncSpeed.Fast:
                     UpdateWhenAvailable();
@@ -175,49 +92,22 @@ namespace HintServiceMeow.Core.Utilities
             }
         }
 
-        /// <summary>
-        /// Update as soon as possible.
-        /// </summary>
-        private void UpdateWhenAvailable(bool useFastUpdate = false)
+        private void UpdateWhenAvailable()
         {
-            lock (_rateDataLock)
-            {
-                TimeSpan timeToWait = useFastUpdate ? FastUpdateCoolDown : UpdateCoolDown;
+            DateTime newUpdateTime = _updateScheduler.UpdateWhenAvailable();
 
-                var newPlanUpdateTime = DateTime.Now + timeToWait + TimeSpan.FromMilliseconds(5);
+            if (newUpdateTime < _planUpdateTime)
+                _planUpdateTime = newUpdateTime;
 
-                if (_planUpdateTime > newPlanUpdateTime)
-                    _planUpdateTime = newPlanUpdateTime;
-            }
+            _planUpdateTime = _updateScheduler.UpdateWhenAvailable();
         }
 
-        /// <summary>
-        /// Arrange an update based on all hint's estimated update time.
-        /// </summary>
-        private void ArrangeUpdate(TimeSpan maxDelay, AbstractHint hint)
+        private void ArrangeUpdate(TimeSpan maxWaitingTime, AbstractHint hint)
         {
-            try
-            {
-                var now = DateTime.Now;
+            DateTime newArrangeUpdateTime = _updateScheduler.ArrangeUpdate(maxWaitingTime, hint, _hints);
 
-                //Find the latest estimated update time within maxDelay
-                DateTime newTime = _hints.AllHints
-                    .Where(h => h.SyncSpeed >= hint.SyncSpeed && h != hint)
-                    .Select(h => h.Analyser.EstimateNextUpdate())
-                    .Where(x => x - now >= TimeSpan.Zero && x - now <= maxDelay)
-                    .DefaultIfEmpty(DateTime.Now)
-                    .Max();
-
-                lock (_rateDataLock)
-                {
-                    if (_arrangedUpdateTime > newTime)
-                        _arrangedUpdateTime = newTime;
-                }
-            }
-            catch (Exception ex)
-            {
-                Log.Error(ex.ToString());
-            }
+            if(newArrangeUpdateTime < _arrangedUpdateTime)
+                _arrangedUpdateTime = newArrangeUpdateTime;
         }
 
         /// <summary>
@@ -233,16 +123,13 @@ namespace HintServiceMeow.Core.Utilities
                     {
                         foreach (PlayerDisplay pd in PlayerDisplayList)
                         {
-                            lock (pd._rateDataLock)
-                            {
-                                //Force update
-                                if (pd.NeedPeriodicUpdate && pd._currentParserTask == null)
-                                    pd.StartParserTask();
+                            //Force update
+                            if (pd._updateScheduler.NeedPeriodicUpdate && pd._currentParserTask == null)
+                                pd.StartParserTask();
 
-                                //Invoke UpdateAvailable event
-                                if (pd.UpdateReady)
-                                    pd.UpdateAvailable?.Invoke(new UpdateAvailableEventArg(pd));
-                            }
+                            //Invoke UpdateAvailable event
+                            if (pd._updateScheduler.UpdateReady)
+                                pd.UpdateAvailable?.Invoke(new UpdateAvailableEventArg(pd));
                         }
                     }
                 }
@@ -270,51 +157,38 @@ namespace HintServiceMeow.Core.Utilities
                     {
                         foreach (PlayerDisplay pd in PlayerDisplayList)
                         {
-                            lock (pd._rateDataLock)
+                            //Check arranged update time.
+                            if (now > pd._arrangedUpdateTime)
                             {
-                                //Check arranged update time.
-                                if (now > pd._arrangedUpdateTime)
-                                {
-                                    pd._arrangedUpdateTime = DateTime.MaxValue;
-                                    pd.UpdateWhenAvailable();
-                                }
-
-                                //Update based on plan
-                                //If last update complete, then start a new update. Otherwise, wait for the last update to complete
-                                if (now > pd._planUpdateTime && pd._currentParserTask == null)
-                                {
-                                    //Reset Update Plan
-                                    pd._planUpdateTime = DateTime.MaxValue;
-                                    pd._updatingHints.Clear();
-
-                                    pd.StartParserTask();
-                                }
+                                pd._arrangedUpdateTime = DateTime.MaxValue;
+                                pd.UpdateWhenAvailable();
                             }
 
-                            //If competed parsing, send the parsed text to client
+                            //Update based on plan
+                            //If last update complete, then start a new update. Otherwise, wait for the last update to complete
+                            if (now > pd._planUpdateTime && pd._currentParserTask == null)
+                            {
+                                //Reset Update Plan
+                                pd._planUpdateTime = DateTime.MaxValue;
+                                pd.StartParserTask();
+                            }
+
+                            //If complete parsing, send the parsed text to client
                             if (pd._currentParserTask != null && pd._currentParserTask.IsCompleted)
                             {
                                 var text = pd._currentParserTask.GetAwaiter().GetResult();
-                                bool shouldUpdate = false;
 
-                                lock (pd._rateDataLock)
+                                //Only update when text had changed since last update or if this is a force update
+                                if (text != pd._lastText || pd._updateScheduler.NeedPeriodicUpdate)
                                 {
-                                    //Only update when text had changed since last update or if this is a force update
-                                    if (text != pd._lastText || pd.NeedPeriodicUpdate)
-                                    {
-                                        //Update text record
-                                        pd._lastText = text;
+                                    //Update text record
+                                    pd._lastText = text;
 
-                                        //Reset CountDown
-                                        pd._secondLastTimeUpdate = pd._lastTimeUpdate;
-                                        pd._lastTimeUpdate = DateTime.Now;
+                                    //Reset CountDown
+                                    pd._updateScheduler.Reset();
 
-                                        shouldUpdate = true;
-                                    }
-                                }
-
-                                if (shouldUpdate)
                                     pd.SendHint(text);
+                                }
 
                                 pd._currentParserTask.Dispose();
                                 pd._currentParserTask = null;
@@ -341,7 +215,7 @@ namespace HintServiceMeow.Core.Utilities
         /// <param name="useFastUpdate">Use fast update might increase the cooldown of next update</param>
         public void ForceUpdate(bool useFastUpdate = false)
         {
-            UpdateWhenAvailable(useFastUpdate);
+            UpdateWhenAvailable();
         }
 
         private void StartParserTask()
---
+++ HintServiceMeow/Core/Utilities/UpdateScheduler.cs
@@ -0,0 +1,126 @@
+﻿using HintServiceMeow.Core.Enum;
+using HintServiceMeow.Core.Models.Hints;
+using MEC;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PluginAPI.Core;
+using static HintServiceMeow.Core.Utilities.PlayerDisplay;
+using HintServiceMeow.Core.Models;
+
+namespace HintServiceMeow.Core.Utilities
+{
+    internal class UpdateScheduler
+    {
+        private readonly object _rateDataLock = new object();
+
+        /// <summary>
+        /// The time of latest update
+        /// </summary>
+        private DateTime _lastTimeUpdate = DateTime.MinValue;
+
+        /// <summary>
+        /// The time of second-last update
+        /// </summary>
+        private DateTime _secondLastTimeUpdate = DateTime.MinValue;
+
+        /// <summary>
+        /// The minimum interval between a regular update and the last update
+        /// </summary>
+        public static readonly TimeSpan UpdateInterval = TimeSpan.FromSeconds(0.5f);
+
+        /// <summary>
+        /// The minimum interval between a force update and the second last update
+        /// </summary>
+        public static readonly TimeSpan SecondLastUpdateInterval = TimeSpan.FromSeconds(1f);
+
+        /// <summary>
+        /// The player display will be forced to update when there haven't been any update in this amount of time
+        /// </summary>
+        public static readonly TimeSpan PeriodicUpdateInterval = TimeSpan.FromSeconds(5f);
+
+        /// <summary>
+        /// The time left for regular update to cool down.
+        /// </summary>
+        public TimeSpan UpdateCoolDown
+        {
+            get
+            {
+                TimeSpan delay1 = (this._lastTimeUpdate + UpdateInterval - DateTime.Now);
+                TimeSpan delay2 = (this._secondLastTimeUpdate + SecondLastUpdateInterval - DateTime.Now);
+                return delay1 > delay2 ? delay1 : delay2;
+            }
+        }
+
+        /// <summary>
+        /// Whether regular update is ready
+        /// </summary>
+        public bool UpdateReady => UpdateCoolDown.Ticks <= 0;
+
+        /// <summary>
+        /// The time left for fast update to cool down.
+        /// </summary>
+        public TimeSpan FastUpdateCoolDown => this._secondLastTimeUpdate + SecondLastUpdateInterval - DateTime.Now;
+
+        /// <summary>
+        /// Whether fast update is ready
+        /// </summary>
+        public bool FastUpdateReady => FastUpdateCoolDown.Ticks <= 0;
+
+        /// <summary>
+        /// The time PlayerDisplay will execute a periodic update
+        /// </summary>
+        public TimeSpan PeriodicUpdateCoolDown => _lastTimeUpdate + PeriodicUpdateInterval - DateTime.Now;
+
+        /// <summary>
+        /// Whether PlayerDisplay need to force update or not
+        /// </summary>
+        public bool NeedPeriodicUpdate => PeriodicUpdateCoolDown.Ticks <= 0;
+
+        /// <summary>
+        /// Return the next time when an update is available
+        /// </summary>
+        public DateTime UpdateWhenAvailable(bool useFastUpdate = false)
+        {
+            lock (_rateDataLock)
+            {
+                TimeSpan timeToWait = useFastUpdate ? FastUpdateCoolDown : UpdateCoolDown;
+
+                return DateTime.Now + timeToWait + TimeSpan.FromMilliseconds(5);
+            }
+        }
+
+        /// <summary>
+        /// Return the next time when an update should be arranged
+        /// </summary>
+        public DateTime ArrangeUpdate(TimeSpan maxDelay, AbstractHint hint, HintCollection hints)
+        {
+            try
+            {
+                var now = DateTime.Now;
+
+                //Find the latest estimated update time within maxDelay
+                return hints.AllHints
+                    .Where(h => h.SyncSpeed >= hint.SyncSpeed && h != hint)
+                    .Select(h => h.Analyser.EstimateNextUpdate())
+                    .Where(x => x - now >= TimeSpan.Zero && x - now <= maxDelay)
+                    .DefaultIfEmpty(DateTime.Now)
+                    .Max();
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex.ToString());
+            }
+
+            return DateTime.Now;
+        }
+
+        public void Reset()
+        {
+            _secondLastTimeUpdate = _lastTimeUpdate;
+            _lastTimeUpdate = DateTime.Now;
+        }
+    }
+}
