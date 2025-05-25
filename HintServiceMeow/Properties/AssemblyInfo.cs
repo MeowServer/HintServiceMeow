1281c4ef3e2002dcb38a7791361d6ef41b31ed25
MeowServer~ <2679977872@qq.com>
2025-05-25 19:42:15 -0400

    Tweak for nuget and dependencies fix

--- HintServiceMeow/Properties/AssemblyInfo.cs
+++ HintServiceMeow/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("HintServiceMeow")]
 [assembly: AssemblyDescription("A hint framework for SCP:SL")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("MeowServer~")]
+[assembly: AssemblyCompany("MeowServer")]
 [assembly: AssemblyProduct("HintServiceMeow")]
 [assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("5.4.1.0")]
+[assembly: AssemblyVersion("5.4.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
