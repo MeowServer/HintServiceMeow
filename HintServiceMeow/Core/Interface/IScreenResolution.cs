4375af43870dcf75716e1e5ed2d62cb3d1160731
MeowServer~ <2679977872@qq.com>
2026-03-17 20:38:24 -0400

    Add IScreenResolution and ScpslScreenResolution

---
+++ HintServiceMeow/Core/Interface/IScreenResolution.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+
+namespace HintServiceMeow.Core.Interface
+{
+    internal interface IScreenResolution : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// Gets the value of the X coordinate divided by Y coordinate.
+        /// </summary>
+        /// <returns>A floating-point value representing the ratio of the X coordinate to the Y coordinate.</returns>
+        float XyRatio { get; }
+    }
+}
---
+++ HintServiceMeow/Core/Models/UniryAdaptors/ScpslScreenResolution.cs
@@ -0,0 +1,49 @@
+﻿namespace HintServiceMeow.Core.Models.UniryAdaptors
+{
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using HintServiceMeow.Core.Interface;
+    using MEC;
+    using UnityEngine;
+
+    internal class ScpslScreenResolution : IScreenResolution
+    {
+        private static float yScreenEdge = 0;
+        private AspectRatioSync ratioSync;
+        private float xScreenEdge;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ScpslScreenResolution"/> class.
+        /// Not Thread Safe. 
+        /// </summary>
+        /// <param name="referenceHub">The ReferenceHub instance that provides aspect ratio synchronization settings for the screen resolution.</param>
+        public ScpslScreenResolution(ReferenceHub referenceHub)
+        {
+            if (yScreenEdge == 0)
+            {
+                yScreenEdge = AspectRatioSync.YScreenEdge;
+            }
+
+            ratioSync = referenceHub.aspectRatioSync;
+            Timing.RunCoroutine(GetRatioEnumerator());
+        }
+
+        public float XyRatio => Mathf.Tan(xScreenEdge * Mathf.Deg2Rad) / Mathf.Tan(yScreenEdge * Mathf.Deg2Rad);
+
+        private IEnumerator<float> GetRatioEnumerator()
+        {
+            while (true)
+            {
+                if (xScreenEdge != ratioSync.XScreenEdge)
+                {
+                    xScreenEdge = ratioSync.XScreenEdge;
+                    PropertyChanged.Invoke(this, new PropertyChangedEventArgs(nameof(XyRatio)));
+                }
+
+                yield return Timing.WaitForOneFrame;
+            }
+        }
+    }
+}
