4e16e3f4207435cc26cee64132f3d6b79a0b02c9
MeowServer~ <2679977872@qq.com>
2026-03-18 17:40:54 -0400

    Remove unused ResolutionOption

--- HintServiceMeow/Core/Enum/ResolutionOption.cs
+++ HintServiceMeow/Core/Enum/ResolutionOption.cs
@@ -8,15 +8,5 @@
         /// Give a offset to XCoordinate that push your hint to the edge of the screen if your hint has a alignment of left or right.
         /// </summary>
         Offset,
-
-        /// <summary>
-        /// Scale your XCoordinate according to screen aspect ratio, making your hint stays at the same relative position to the center of the screen.
-        /// </summary>
-        Scale,
-
-        /// <summary>
-        /// Give a offset to XCoordinate and scale it according to screen aspect ratio. This method combined Offset and Scale options.
-        /// </summary>
-        OffsetAndScale,
     }
 }
