14e12a38ec163eec029184cd81bf000068e80f5b
MeowServer~ <2679977872@qq.com>
2026-03-18 12:39:36 -0400

    Add update coroutine and event to ScpslScreenResolution

--- HintServiceMeow/Core/Models/UniryAdaptors/ScpslScreenResolution.cs
+++ HintServiceMeow/Core/Models/UniryAdaptors/ScpslScreenResolution.cs
@@ -8,9 +8,14 @@
 
     internal class ScpslScreenResolution : IScreenResolution
     {
+        private static readonly object StaticStatusLock = new();
+        private static readonly List<ScpslScreenResolution> Instances = new();
+        private static CoroutineHandle coroutineHandle;
+
         private static float yScreenEdge = 0;
-        private AspectRatioSync ratioSync;
-        private float xScreenEdge;
+        private readonly ReferenceHub? referenceHub;
+        private volatile float xScreenEdge;
+        private volatile float xyRatio;
 
         public event PropertyChangedEventHandler PropertyChanged;
 
@@ -21,28 +26,55 @@
         /// <param name="referenceHub">The ReferenceHub instance that provides aspect ratio synchronization settings for the screen resolution.</param>
         public ScpslScreenResolution(ReferenceHub referenceHub)
         {
-            if (yScreenEdge == 0)
+            lock (StaticStatusLock)
             {
-                yScreenEdge = AspectRatioSync.YScreenEdge;
-            }
+                if (yScreenEdge == 0)
+                {
+                    yScreenEdge = AspectRatioSync.YScreenEdge;
+                }
 
-            ratioSync = referenceHub.aspectRatioSync;
-            Timing.RunCoroutine(GetRatioEnumerator());
+                this.referenceHub = referenceHub;
+                if (!coroutineHandle.IsRunning)
+                {
+                    coroutineHandle = Timing.RunCoroutine(CoroutineMethod());
+                }
+
+                Instances.Add(this);
+            }
         }
 
-        public float XyRatio => Mathf.Tan(xScreenEdge * Mathf.Deg2Rad) / Mathf.Tan(yScreenEdge * Mathf.Deg2Rad);
+        public float XyRatio => xyRatio;
 
-        private IEnumerator<float> GetRatioEnumerator()
+        private static IEnumerator<float> CoroutineMethod()
         {
             while (true)
             {
-                if (xScreenEdge != ratioSync.XScreenEdge)
+                List<ScpslScreenResolution> updatedInstances = new();
+
+                lock (StaticStatusLock)
                 {
-                    xScreenEdge = ratioSync.XScreenEdge;
-                    PropertyChanged.Invoke(this, new PropertyChangedEventArgs(nameof(XyRatio)));
+                    if (Instances.Count == 0)
+                        yield break;
+
+                    Instances.RemoveAll(x => x.referenceHub == null);
+
+                    foreach (ScpslScreenResolution resolution in Instances)
+                    {
+                        if (resolution.xScreenEdge != resolution.referenceHub!.aspectRatioSync.XScreenEdge)
+                        {
+                            resolution.xScreenEdge = resolution.referenceHub.aspectRatioSync.XScreenEdge;
+                            resolution.xyRatio = Mathf.Tan(resolution.xScreenEdge * Mathf.Deg2Rad) / Mathf.Tan(yScreenEdge * Mathf.Deg2Rad);
+                            updatedInstances.Add(resolution);
+                        }
+                    }
                 }
 
-                yield return Timing.WaitForOneFrame;
+                foreach (ScpslScreenResolution resolution in updatedInstances)
+                {
+                    resolution.PropertyChanged?.Invoke(resolution, new PropertyChangedEventArgs(nameof(XyRatio)));
+                }
+
+                yield return Timing.WaitForSeconds(1f);
             }
         }
     }
